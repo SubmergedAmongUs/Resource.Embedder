f3bb10aae62aa25700b1d6d49f5eb5168abb664e
Marc Stan <marcstan@live.com>
2019-09-20 20:16:18 +0200

    add build pipeline with ability to push to nuget

--- src/AssemblyInfoInc.cs
+++
@@ -1,6 +0,0 @@
-﻿using System.Reflection;
-
-[assembly: AssemblyVersion("2.0")]
-[assembly: AssemblyFileVersion("2.0.1.0")]
-[assembly: AssemblyCompany("MarcStan")]
-[assembly: AssemblyCopyright("Copyright © MarcStan 2015 - 2019")]
